b8506cfb8a49f1b2e7a26b48ff86e2cbb7479652
szamba <sebastian.zambal@profactor.at>
2020-09-26 13:42:37 +0200

    Attempt to use BRDF randomization (as used in branch camera_noise, but with real background image) - not working for some reason

--- run.bat
+++ run.bat
@@ -4,11 +4,11 @@ REM tscon 1 /dest:console
 python record.py --num-items 50000 --json-config d:\dev\pytorch-blender-dr\blender\tless\bgImgConfig.json tless
 REM python record.py --num-items 20 --json-config d:\dev\pytorch-blender-dr\blender\tless\bgImgConfig.json tless
 
-xcopy /Y .\tmp\*.btr \\gpusrv.profactor.local\data\20200923_TLess_realBgImagesConfigFixed\
+xcopy /Y .\tmp\*.btr \\gpusrv.profactor.local\data\20200924_TLess_asInNoiseBranchButRealBGImages\
 
-xcopy /Y .\tmp\output_0.png \\gpusrv.profactor.local\data\20200923_TLess_realBgImagesConfigFixed\
-xcopy /Y .\tmp\output_100.png \\gpusrv.profactor.local\data\20200923_TLess_realBgImagesConfigFixed\
-xcopy /Y .\tmp\output_1000.png \\gpusrv.profactor.local\data\20200923_TLess_realBgImagesConfigFixed\
-xcopy /Y .\tmp\output_10000.png \\gpusrv.profactor.local\data\20200923_TLess_realBgImagesConfigFixed\
+xcopy /Y .\tmp\output_0.png \\gpusrv.profactor.local\data\20200924_TLess_asInNoiseBranchButRealBGImages\
+xcopy /Y .\tmp\output_100.png \\gpusrv.profactor.local\data\20200924_TLess_asInNoiseBranchButRealBGImages\
+xcopy /Y .\tmp\output_1000.png \\gpusrv.profactor.local\data\20200924_TLess_asInNoiseBranchButRealBGImages\
+xcopy /Y .\tmp\output_10000.png \\gpusrv.profactor.local\data\20200924_TLess_asInNoiseBranchButRealBGImages\
 
-xcopy /Y d:\dev\pytorch-blender-dr\blender\tless\bgImgConfig.json \\gpusrv.profactor.local\data\20200923_TLess_realBgImagesConfigFixed\
+xcopy /Y d:\dev\pytorch-blender-dr\blender\tless\bgImgConfig.json \\gpusrv.profactor.local\data\20200924_TLess_asInNoiseBranchButRealBGImages\
